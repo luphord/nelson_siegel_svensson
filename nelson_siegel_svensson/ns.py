02eedc4a4cc7a8c61931e8d892045020f9a4e3cb
luphord <luphord@protonmail.com>
2019-02-06 06:37:42 +0100

    shifting to 0-based parameter indices

--- nelson_siegel_svensson/ns.py
+++ nelson_siegel_svensson/ns.py
@@ -9,9 +9,9 @@ EPS = np.finfo(float).eps
 
 @dataclass
 class NelsonSiegelCurve:
+    beta0: float
     beta1: float
     beta2: float
-    beta3: float
     tau: float
 
     def factors(self, T):
@@ -22,17 +22,17 @@ class NelsonSiegelCurve:
             zero_idx = T <= 0
             T[zero_idx] = EPS  # avoid warnings in calculations
         exp_tt0 = exp(-T/tau)
-        factor_2 = (1 - exp_tt0) / (T / tau)
-        factor_3 = factor_2 - exp_tt0
+        factor_1 = (1 - exp_tt0) / (T / tau)
+        factor_2 = factor_1 - exp_tt0
         if isinstance(T, np.ndarray):
             T[zero_idx] = 0
-            factor_2[zero_idx] = 1
-            factor_3[zero_idx] = 0
-        return factor_2, factor_3
+            factor_1[zero_idx] = 1
+            factor_2[zero_idx] = 0
+        return factor_1, factor_2
 
     def zero(self, T):
-        factor_2, factor_3 = self.factors(T)
-        return self.beta1 + self.beta2*factor_2 + self.beta3*factor_3
+        factor_1, factor_2 = self.factors(T)
+        return self.beta0 + self.beta1*factor_1 + self.beta2*factor_2
 
     def __call__(self, T):
         return self.zero(T)
