b37e8614687082e97e4a05ac24dbd2f5cf3db057
luphord <luphord@protonmail.com>
2019-02-07 17:22:14 +0100

    proper test modules

---
+++ tests/test_nelson_siegel_curve_implementation.py
@@ -0,0 +1,33 @@
+import unittest
+
+import numpy as np
+
+from nelson_siegel_svensson import NelsonSiegelCurve
+
+
+class TestNelsonSiegelCurveImplementation(unittest.TestCase):
+    '''Tests for Nelson-Siegel curve implementation'''
+
+    def setUp(self):
+        self.y = NelsonSiegelCurve(0.017, -0.023, 0.24, 2.2)
+
+    def test_curve_init(self):
+        NelsonSiegelCurve(0, 0, 0, 0)
+
+    def test_nelson_siegel_curve_array_vs_individual(self):
+        '''Test curve evaluation on array vs individual elements'''
+        t = np.linspace(0, 10, 11)
+        y_array = self.y(t)
+        y_individual = np.array([self.y(t_i) for t_i in t])
+        self.assertTrue(np.allclose(y_array, y_individual),
+                        'array valued yields differ from individual ones')
+
+    def test_nelson_siegel_at_0(self):
+        '''Test curve evaluation at time 0'''
+        y = self.y
+        y_actual = y(0.0)  # float time
+        self.assertEqual(y.beta0 + y.beta1, y_actual)
+        y_actual = y(0)  # int time
+        self.assertEqual(y.beta0 + y.beta1, y_actual)
+        y_actual = y(np.array([0.0]))  # array time
+        self.assertEqual(y.beta0 + y.beta1, y_actual[0])
