ae1242f432185dddb941d3077558813b5252bef2
luphord <luphord@protonmail.com>
2019-02-18 06:41:56 +0100

    test ols based error function for correct tau

--- tests/test_nelson_siegel_calibration.py
+++ tests/test_nelson_siegel_calibration.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 
 from nelson_siegel_svensson import NelsonSiegelCurve
-from nelson_siegel_svensson.calibrate import betas_ns_ols
+from nelson_siegel_svensson.calibrate import betas_ns_ols, errorfn_ns_ols
 
 
 class TestNelsonSiegelCurveCalibration(unittest.TestCase):
@@ -22,3 +22,12 @@ class TestNelsonSiegelCurveCalibration(unittest.TestCase):
         self.assertAlmostEqual(self.y.beta0, curve.beta0, places=12)
         self.assertAlmostEqual(self.y.beta1, curve.beta1, places=12)
         self.assertAlmostEqual(self.y.beta2, curve.beta2, places=12)
+
+    def test_nelson_siegel_ols_errorfn_for_correct_tau(self):
+        '''Test ols based error function for correct tau'''
+        t = np.linspace(0, 30)
+        y_target = self.y(t)
+        error = errorfn_ns_ols(self.y.tau, t, y_target)
+        self.assertAlmostEqual(0.0, error, places=12)
+        error2 = errorfn_ns_ols(self.y.tau * 1.1, t, y_target)
+        self.assertNotAlmostEqual(0.0, error2)
