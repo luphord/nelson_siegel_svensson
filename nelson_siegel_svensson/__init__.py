a325b5edd7a56cacaca6477f8af4c9164cc2ea7d
luphord <luphord@protonmail.com>
2019-02-20 07:01:50 +0100

    Bump version: 0.1.0 → 0.2.0

--- nelson_siegel_svensson/__init__.py
+++ nelson_siegel_svensson/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """luphord"""
 __email__ = 'luphord@protonmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .ns import NelsonSiegelCurve
 from .nss import NelsonSiegelSvenssonCurve
