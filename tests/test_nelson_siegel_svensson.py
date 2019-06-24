b6818cbd84def6c662a84f770ac8b41b653f5a1e
luphord <luphord@protonmail.com>
2019-06-24 17:14:52 +0200

    more parameter tests

--- tests/test_nelson_siegel_svensson.py
+++ tests/test_nelson_siegel_svensson.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import json
+from dataclasses import asdict
 import click
 from click.testing import CliRunner
 
-from nelson_siegel_svensson import cli
+from nelson_siegel_svensson import cli, NelsonSiegelCurve, \
+    NelsonSiegelSvenssonCurve
 
 
 class TestNelson_siegel_svensson(unittest.TestCase):
-    '''Tests for `nelson_siegel_svensson` package.'''
+    '''Tests for `nelson_siegel_svensson` CLI.'''
+
+    def setUp(self):
+        self.y1 = NelsonSiegelCurve(0.017, -0.023, 0.24, 2.2)
+        self.y2 = NelsonSiegelSvenssonCurve(0.017, -0.023, 0.24, 0.1, 2.2, 3.1)
 
     def test_command_line_interface(self):
         '''Test the CLI.'''
@@ -24,3 +31,14 @@ class TestNelson_siegel_svensson(unittest.TestCase):
         param = cli.Curve()
         self.assertRaises(click.BadParameter, param.convert,
                           value='', param=None, ctx=None)
+        self.assertRaises(click.BadParameter, param.convert,
+                          value='{}', param=None, ctx=None)
+        missing_tau = '{"beta0": 0.017, "beta1": -0.023, "beta2": 0.24}'
+        self.assertRaises(click.BadParameter, param.convert,
+                          value=missing_tau, param=None, ctx=None)
+        self.assertEqual(self.y1,
+                         param.convert(json.dumps(asdict(self.y1)),
+                                       None, None))
+        self.assertEqual(self.y2,
+                         param.convert(json.dumps(asdict(self.y2)),
+                                       None, None))
