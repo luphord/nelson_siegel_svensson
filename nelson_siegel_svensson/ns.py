7bfa807d21a4935ee1d1387f23ae0402e7096dc9
luphord <luphord@protonmail.com>
2019-11-14 06:50:07 +0100

    add missing return type annotations for ns

--- nelson_siegel_svensson/ns.py
+++ nelson_siegel_svensson/ns.py
@@ -6,7 +6,7 @@ See `NelsonSiegelCurve` class for details.
 
 from numbers import Real
 from dataclasses import dataclass
-from typing import Union
+from typing import Union, Tuple
 
 import numpy as np
 from numpy import exp
@@ -26,7 +26,8 @@ class NelsonSiegelCurve:
     beta2: float
     tau: float
 
-    def factors(self, T: Union[Real, np.ndarray]):
+    def factors(self, T: Union[Real, np.ndarray]) \
+            -> Union[Tuple[Real, Real], Tuple[np.ndarray, np.ndarray]]:
         '''Factor loadings for time(s) T, excluding constant.'''
         tau = self.tau
         if isinstance(T, Real) and T <= 0:
@@ -43,7 +44,8 @@ class NelsonSiegelCurve:
             factor2[zero_idx] = 0
         return factor1, factor2
 
-    def factor_matrix(self, T: Union[Real, np.ndarray]):
+    def factor_matrix(self, T: Union[Real, np.ndarray]) \
+            -> Union[Real, np.ndarray]:
         '''Factor loadings for time(s) T as matrix columns,
            including constant column (=1.0).
         '''
@@ -51,16 +53,16 @@ class NelsonSiegelCurve:
         constant = np.ones(T.size) if isinstance(T, np.ndarray) else 1
         return np.stack([constant, factor1, factor2]).transpose()
 
-    def zero(self, T: Union[Real, np.ndarray]):
+    def zero(self, T: Union[Real, np.ndarray]) -> Union[Real, np.ndarray]:
         '''Zero rate(s) of this curve at time(s) T.'''
         factor1, factor2 = self.factors(T)
         return self.beta0 + self.beta1*factor1 + self.beta2*factor2
 
-    def __call__(self, T: Union[Real, np.ndarray]):
+    def __call__(self, T: Union[Real, np.ndarray]) -> Union[Real, np.ndarray]:
         '''Zero rate(s) of this curve at time(s) T.'''
         return self.zero(T)
 
-    def forward(self, T: Union[Real, np.ndarray]):
+    def forward(self, T: Union[Real, np.ndarray]) -> Union[Real, np.ndarray]:
         '''Instantaneous forward rate(s) of this curve at time(s) T.'''
         exp_tt0 = exp(-T/self.tau)
         return self.beta0 + self.beta1*exp_tt0 + self.beta2*exp_tt0*T/self.tau
