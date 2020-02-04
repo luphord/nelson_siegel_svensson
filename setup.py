db13da5dc03e0d8f988f2f7f6261cd6dee7db7fb
luphord <luphord@protonmail.com>
2020-02-04 06:53:43 +0100

    support python 3.8

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     description='Implementation of the Nelson-Siegel-Svensson ' +
                 'interest rate curve model.',
