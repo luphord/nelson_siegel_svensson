ea82a6b66699442b4064fc9ccbac716ee97b35c6
luphord <luphord@protonmail.com>
2022-03-15 22:01:57 +0100

    Bump version: 0.4.2 → 0.4.3

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/luphord/nelson_siegel_svensson',
-    version='0.4.2',
+    version='0.4.3',
     zip_safe=False,
 )
