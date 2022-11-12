282153d74393b6807608d4813b4594bbd979c36f
luphord <luphord@protonmail.com>
2022-11-12 20:13:02 +0100

    test experimental constrained optimization for Nelson-Siegel

--- tests/test_nelson_siegel_calibration.py
+++ tests/test_nelson_siegel_calibration.py
@@ -3,6 +3,7 @@
 import unittest
 
 import numpy as np
+from scipy.optimize import minimize
 
 from nelson_siegel_svensson import NelsonSiegelCurve
 from nelson_siegel_svensson.calibrate import (
@@ -77,3 +78,21 @@ class TestNelsonSiegelCurveCalibration(unittest.TestCase):
         self.assertAlmostEqual(self.y.beta1, y_hat.beta1, places=places_beta)
         self.assertAlmostEqual(self.y.beta2, y_hat.beta2, places=places_beta)
         self.assertAlmostEqual(self.y.tau, y_hat.tau, places=places_tau)
+
+    def test_nelson_siegel_ols_constrained_calibration(self):
+        """Test ols based calibration with constrained optimization of tau
+        of Nelson-Siegel model."""
+        t = np.linspace(0, 30)
+        y_target = self.y(t)
+        tau0_grid = (0.08, 0.1, 0.2, 0.5, 1, 2, 3, 4, 5)
+        places_tau = 2
+        # unconstrained
+        for tau0 in tau0_grid:
+            opt_res = minimize(errorfn_ns_ols, x0=tau0, args=(t, y_target))
+            self.assertAlmostEqual(self.y.tau, opt_res.x[0], places=places_tau)
+        # constrained
+        for tau0 in tau0_grid:
+            opt_res = minimize(
+                errorfn_ns_ols, x0=tau0, args=(t, y_target), bounds=((0.08, None),)
+            )
+            self.assertAlmostEqual(self.y.tau, opt_res.x[0], places=places_tau)
